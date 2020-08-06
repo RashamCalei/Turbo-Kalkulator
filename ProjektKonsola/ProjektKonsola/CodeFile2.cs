0fef4384832c1cfb5e4b63edd0da338e7a2b2520
Rasham Calei <rog.rasham@gmail.com>
2020-08-06 23:01:05 +0200

    Deleted code from previous commit Added working switch..case to choose calculator mode Added working parsing method Parser() Need to implement security if-s

--- ProjektKonsola/ProjektKonsola/CodeFile2.cs
+++ ProjektKonsola/ProjektKonsola/CodeFile2.cs
@@ -1,38 +1,29 @@
 ﻿using System;
 public class Program
 {
+    public static int Parser(string input)
+    {
+        int a = int.Parse(input);
+        return a;
+    }
     public static void Main()
     {
-        Console.WriteLine("Kalkulator");
-        double a;
-        double b;
-        double c;
-        string load = "";
-        do          // nadanie wartości  parsowanie a
+        Console.WriteLine("Witaj w programie Kalkulator");
+        Console.WriteLine("Podaj tryb kalkulatora: ");
+        Console.WriteLine("[1] - Standard\n[2] - Matura");
+    label1:;
+        string mode = Console.ReadLine();
+        switch (Parser(mode))
         {
-            Console.WriteLine("Podaj a");
-            try
-            {
-                load = Console.ReadLine();
-                double.TryParse(load, out a );
-            }
-            catch(Exception e)      // ciało catch się  nie odpala w przypadku błędnych danych
-            {
-                Console.WriteLine("Zła wartość, proszę podać liczbę.");
-            }
-        } while (double.TryParse(load,  out a) == false);
-        do          // nadanie wartości parsowanie  b
-        {
-            Console.WriteLine("Podaj b");
-            try
-            {
-                load = Console.ReadLine();
-                double.TryParse(load, out b);
-            }
-            catch       // nieważne czy z (Exception e) czy bez
-            {
-                Console.WriteLine("Zła wartość, proszę podać liczbę.");
-            }
-        } while (double.TryParse(load, out b) == false);
+            case 1:
+                { Console.WriteLine("coś"); break; }
+            case 2:
+                { break; }
+            default:
+                {
+                   goto label1;
+                }
+        }
+
     }
 }
