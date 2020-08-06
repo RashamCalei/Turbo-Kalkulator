0a58005ec10170e8e086857c2b3659f7d60ec664
Rasham Calei <rog.rasham@gmail.com>
2020-08-06 07:34:33 +0200

    Revert "Added parsing method prototype"
    This reverts commit a1d1ad81684936913db4dda598f19fae2f987614.
    starting from scratch - the most raw form

--- ProjektKonsola/ProjektKonsola/CodeFile1.cs
+++ ProjektKonsola/ProjektKonsola/CodeFile1.cs
@@ -1,32 +1,5 @@
 ﻿using System;
-public class Calculator
-{
-    private double a;
-    private double b;
-}
 public class Program
 {
-    public static double Parser(string text)
-    {
-        double a = 0;
-        try
-        {
-            a = double.Parse(text);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("To nie cyfra! Proszę podać prawidłową wartość.");
-        }
-        return a;
-    }
 
-    public static void Main()
-    {
-        Console.WriteLine("Witaj w TurboCalculator - Console Edition!\nWersja standardowa jest ustawiona domyślnie.");
-        Console.WriteLine("Proszę wcisnąć 'T' aby przejść do werjsi Turbo.");
-        //if ()
-        /*string getvalue = Console.ReadLine();
-        double a = Parser(getvalue);
-        Console.WriteLine("a = " + a);*/
-    }
 }
